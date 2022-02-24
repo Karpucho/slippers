c15ff0481a090e1615e1c3b6bba52ea539737f42
Ernis <e.badmaev@gmail.com>
2022-02-24 11:45:34 +0300

    reConfigPackage

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,6 @@
 const router = require("express").Router();
-const userRouter = require("./user.router");
-// const dogRouter = require("./d.router");
 const authRouter = require("./auth.router");
-const logoutRouter = require("./logout.router");
 
-router.use("/api/user", userRouter);
-// router.use("/api/dog", dogRouter);
 router.use("/", authRouter);
-router.use("/logout", logoutRouter);
 
 module.exports = router;
