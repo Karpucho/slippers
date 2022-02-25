20a14c7390ecce13d5d2513fb6a17adc739a23b3
Ernis <e.badmaev@gmail.com>
2022-02-25 17:36:09 +0300

    finalConfig

--- server/app.js
+++ server/app.js
@@ -2,48 +2,32 @@ const express = require("express");
 const dotenv = require("dotenv");
 const config = require("./config/config");
 const router = require("./routes/index");
-const productsRouter = require("./routes/products.router");
-const errorHandler = require("./middleware/errorHandler");
+// const productsRouter = require("./routes/products.router");
 
 dotenv.config();
 
-const config = require('./config/config');
-// const express = require('express');
-// const cors = require('cors');
-// const cookieParser = require('cookie-parser');
-const {
-  sequelize,
-} = require('./db/models');
+const { sequelize } = require("./db/models");
 
-const router = require('./routes/index');
-const errorMiddleware = require('./middlewares/errors.middleware');
+const errorMiddleware = require("./middlewares/errors.middleware");
 
 const PORT = process.env.PORT ?? 5000;
 const app = express();
 
 config(app);
 
-// app.use(express.json());
-// app.use(cookieParser());
-
-// const corsOptions = {
-//   origin: ['http://localhost:3000', 'http://localhost:5000'],
-//   optionsSuccessStatus: 200,
-//   credentials: true,
-// };
-// app.use(cors(corsOptions));
-
-app.use('/api', router);
+app.use("/api", router);
 // обазательно должен идти послденим
 app.use(errorMiddleware);
 
 app.listen(PORT, async () => {
-  console.log('Сервер запущен на порту', PORT);
+  console.log("Сервер запущен на порту", PORT);
 
   try {
     await sequelize.authenticate();
-    console.log('Подключение к БД успешно');
+    console.log("Подключение к БД успешно");
   } catch {
-    console.log('Не получилось подключиться к БД');
+    console.log("Не получилось подключиться к БД");
   }
 });
+
+//все работает!
