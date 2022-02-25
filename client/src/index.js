8642e18b1c1a8d95d62d226be7ad8343750c819a
Ernis <e.badmaev@gmail.com>
2022-02-25 17:45:10 +0300

    resolveSrc

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import App from './components/App/App.jsx';
 import { Provider } from 'react-redux'
 import { store } from "./redux/store";
-=======
-import App from './components/App/App';
-// import reportWebVitals from './reportWebVitals';
->>>>>>> 0cc64699c1d0cd3f3a02827f984dd0b067d9ec37
 
 ReactDOM.render(
   <Provider store={store}>
@@ -16,10 +11,4 @@ ReactDOM.render(
     </React.StrictMode>
   </Provider>,
   document.getElementById('root')
-<<<<<<< HEAD
 )
-=======
-);
-
-// reportWebVitals();
->>>>>>> 0cc64699c1d0cd3f3a02827f984dd0b067d9ec37
--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,16 +1,7 @@
 import { combineReducers } from 'redux';
-<<<<<<< HEAD:client/src/redux/reducers/rootReducer.js
 // import { usersReducer } from './usersReducer';
 
 export const rootReducer = combineReducers({
 
   // usersReducer,
-=======
-import { productsReducer } from './productsReducer';
-import { usersReducer } from './usersReducer';
-
-export const rootReducer = combineReducers({
-  productsReducer,
-  usersReducer,
->>>>>>> 0cc64699c1d0cd3f3a02827f984dd0b067d9ec37:client/src/redux/redusers/rootReducer.js
 });
