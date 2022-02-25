481d5fbb9fd915b7c2476f74b151d38c3d6ab712
Ernis <e.badmaev@gmail.com>
2022-02-25 18:08:18 +0300

    tap

--- client/src/redux/reducers/rootReducer.js
+++ client/src/redux/reducers/rootReducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-// import { usersReducer } from './usersReducer';
+import { usersReducer } from './usersReducer';
 
 export const rootReducer = combineReducers({
 
-  // usersReducer,
+  usersReducer,
 });
