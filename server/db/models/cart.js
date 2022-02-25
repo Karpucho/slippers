1cf3f1173d3b064e9a7f63f8c261c1d217f230fe
Ernis <e.badmaev@gmail.com>
2022-02-25 20:58:07 +0300

    reConfigDB

--- server/db/models/cart.js
+++ server/db/models/cart.js
@@ -4,10 +4,10 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
   class Cart extends Model {
-    static associate({ Product, User, SizesOfProducts }) {
+    static associate({ Product, User, SizesOfProduct }) {
       Cart.belongsTo(User, { foreignKey: 'userId' });
       Cart.belongsTo(Product, { foreignKey: 'productId' });
-      Cart.belongsTo(SizesOfProducts, { foreignKey: 'sizeId' });
+      Cart.belongsTo(SizesOfProduct, { foreignKey: 'sizeId' });
     }
   }
   Cart.init({
