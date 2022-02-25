2fcf45ec2bed9437c5a0cffddcea215d1f41796f
Ernis <e.badmaev@gmail.com>
2022-02-25 16:26:56 +0300

    createProfile

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,17 +1,14 @@
 const userService = require("../services/user");
 
 async function getUser(req, res) {
-  // console.log(req.params.id);
   try {
     const { id: userId } = req.params;
-    console.log(userId);
     const user = await userService.getUserById(userId);
     res.status(200).json({
       user,
       message: "найденный юзер",
     });
   } catch (error) {
-    console.log(error.message);
     res.status(400).json({
       success: false,
       message: error.message,
@@ -19,24 +16,24 @@ async function getUser(req, res) {
   }
 }
 
-// async function changeUser(req, res) {
-//   try {
-//     const { userId } = req;
-//     await userService.updateUser(userId, req.body);
-//     const user = await userService.getUserById(userId);
-//     res.status(200).json({
-//       user,
-//       message: "данные юзера обновлены",
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       success: false,
-//       message: error.message,
-//     });
-//   }
-// }
+async function changeUser(req, res) {
+  try {
+    const { id: userId } = req.params;
+    await userService.updateUser(userId, req.body);
+    const user = await userService.getUserById(userId);
+    res.status(200).json({
+      user,
+      message: "данные юзера обновлены",
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
 
 module.exports = {
   getUser,
-  // changeUser,
+  changeUser,
 };
