57a93523d8381ff58a1950e12135f4d6a6903580
Ivan Kudryvtcev <ivan-kudryavcev-1998@mail.ru>
2022-02-25 14:26:30 +0300

    navBarFix

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -12,7 +12,6 @@ function App() {
     <Provider store={store}>
       <BrowserRouter>
       <NavBar/>
-        <h1>Test!</h1>
         <Routes>
           <Route path="/home" element={<Home />} />
           <Route path="/signup" element={<RegForm />} />
