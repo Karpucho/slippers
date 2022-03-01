5a58878d294f5cf3e18134722732364655b7563d
Vladimir <vidyag.in@yandex.ru>
2022-03-01 17:14:02 +0300

    it was painful

--- server/routes/upload.route.js
+++ server/routes/upload.route.js
@@ -2,35 +2,33 @@ const router = require('express').Router();
 const fileMiddleware = require('../middlewares/file.middleware');
 const { Product } = require('../db/models');
 
-let idProduct;
-
 router
-  .post('/', fileMiddleware.single('productPhoto'), async (req, res) => {
+  .post('/photo', fileMiddleware.single('productPhoto'), async (req, res) => {
     try {
-      console.log(req.file);
-      const newProduct = await Product.create({
-        photo: `http://localhost:5000/${req.file.path}`,
-      });
-      newProduct.id = idProduct;
-      console.log('idProduct', idProduct);
-
-      res.status(201).json(newProduct);
+      if (req.file) {
+        res.json(req.file);
+      }
     } catch (e) {
       console.log(e);
     }
   })
-  .put('/', async (req, res) => {
+  .post('/', async (req, res) => {
     console.log(req.body);
+
+    const {
+      name, price, photo, gender, description,
+    } = req.body;
+
     try {
-      const newProduct = await Product.findOne({
-        where: { idProduct },
+      const newProduct = await Product.create({
+        name,
+        price,
+        photo: `http://localhost:5000/images/${photo.slice(12)}`,
+        gender,
+        description,
+        status: 'active',
       });
-      newProduct.set(req.body);
-      await newProduct.save();
-      res.json({
-        message: 'vse norm',
-        newProduct,
-      });
+      res.status(201).json(newProduct);
     } catch (error) {
       res.json({ error: error.message });
       console.log(error);
