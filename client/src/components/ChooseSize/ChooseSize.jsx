fcf751355624ce66bd342619e33c927e30c0f406
Ernis <e.badmaev@gmail.com>
2022-03-01 11:32:45 +0300

    reStore

--- client/src/components/ChooseSize/ChooseSize.jsx
+++ client/src/components/ChooseSize/ChooseSize.jsx
@@ -5,18 +5,21 @@ import {Button } from "@material-ui/core";
 function ChooseSize({size, count, setSize}) {
 
 
+  
+
   const chooseSize = (event) => {
     event.preventDefault();
     const btn = event.target.closest('button');
     setSize(btn.textContent);
     btn.style.color = 'green';
   
+    
   }
 
   return (
     <>
     
-    {(+count > 0) && <Button onClick={chooseSize} style={{padding: '0px'}} variant="outlined" color="default">{size}</Button>}
+    {count && <Button onClick={chooseSize} style={{padding: '0px'}} variant="outlined" color="default">{size}</Button>}
 
    </>
 
--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -379,4 +379,3 @@ function NavBar() {
 }
 
 export default NavBar;
-
