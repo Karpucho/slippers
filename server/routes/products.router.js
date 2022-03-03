b915400ad5b19b20979f2e77c9f2ea2daf92dedb
Igor Karpov <karpov.zgr@mail.ru>
2022-03-03 15:06:49 +0300

    addCheckUserDone

--- server/routes/products.router.js
+++ server/routes/products.router.js
@@ -5,20 +5,6 @@ const { Product } = require('../db/models');
 
 router.route('/')
   .get(async (req, res) => {
-
-    
-
-    function parseJwt(token) {
-      const base64Url = token.split('.')[1];
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-      const jsonPayload = decodeURIComponent(atob(base64).split('').map((c) => `%${(`00${c.charCodeAt(0).toString(16)}`).slice(-2)}`).join(''));
-      // console.log(JSON.parse(jsonPayload), 'ТОКЕН');
-      return JSON.parse(jsonPayload);
-    }
-    const token = req.cookies.refreshToken;
-    console.log(parseJwt(token), 'ТОКЕН2');
-
-
     try {
       const products = await Product.findAll({
         // where: { status: 'active' },
