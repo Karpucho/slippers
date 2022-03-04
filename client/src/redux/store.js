80df2c967b2ee7475f4fd2fa3532e0ecba7d46d1
Ernis <e.badmaev@gmail.com>
2022-03-04 14:41:35 +0300

    final

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
 import { rootReducer } from "./reducers/rootReducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import { globalWatcher } from "./sagas/saga";
 
@@ -8,7 +8,8 @@ const saga = createSagaMiddleware();
 
 export const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(saga))
+  // composeWithDevTools
+  applyMiddleware(saga)
 );
 
 saga.run(globalWatcher);
